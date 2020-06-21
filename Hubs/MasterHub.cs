0cb7ba7e59c64d7af2af03764bcd1a9fd1b520af
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-06-21 16:27:51 -0500

    check point

--- Hubs/MasterHub.cs
+++ Hubs/MasterHub.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
+using BusMaster.Model;
 using CoreHambusCommonLibrary.DataLib;
 using CoreHambusCommonLibrary.Services;
 using HambusCommonLibrary;
@@ -16,18 +17,41 @@ namespace BusMaster.Hubs
     {
       GlobalData = globalDb;
     }
+
+    public override async Task OnConnectedAsync()
+    {
+      Console.WriteLine("in OnCnnectedAsync");
+    }
+    public override async Task OnDisconnectedAsync(Exception ex)
+    {
+      Console.WriteLine($"Disconnect: connectionid: {Context.ConnectionId}");
+      //Console.WriteLine($"--> in OnDisconnectedAsync: {ex.Message} <--");
+    }
     public async Task Login(string name, List<string> groups)
     {
+      name = name.ToLower();
       var rigConf = RigConf.Instance;
       Console.WriteLine($"in login: {name}");
       var busConf = new BusConfigurationDB();
       busConf.Id = 20;
       busConf.Name = name;
       busConf.Configuration = JsonSerializer.Serialize(rigConf);
+
+
+ 
       foreach (var group in groups)
       {
         Console.WriteLine($"in groups: {group}");
+        await this.Groups.AddToGroupAsync(this.Context.ConnectionId, group);
       }
+      if (name == "control")
+      {
+        var busPacket = new UiInfoPacketModel();
+        await Clients.Caller.SendAsync("InfoPacket", busPacket);
+        return;
+      }
+
+      await this.Groups.AddToGroupAsync(this.Context.ConnectionId, name);
       await Clients.Caller.SendAsync("ReceiveConfiguration", busConf);
       return;
     }
---
+++ Model/UiInfoPacketModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreHambusCommonLibrary.DataLib;
+
+namespace BusMaster.Model
+{
+  public class UiInfoPacketModel
+  {
+    public List<ActiveBusesModel> ActiveBuses { get; set; } = new List<ActiveBusesModel>();
+    public List<BusConfigurationDB> BusesInDb { get; set; } = new List<BusConfigurationDB>();
+  }
+}
